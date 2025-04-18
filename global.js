79cf62a393c9ac41692ea7a405ad9697b9d9cab7
williamchou500 <williamchou500@gmail.com>
2025-04-17 21:48:56 -0700

    test js

--- global.js
+++ global.js
@@ -3,3 +3,13 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
 }
+
+let navLinks = $$("nav a");
+
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname,
+);
+
+if (currentLink) {
+    currentLink.classList.add('current');
+}
