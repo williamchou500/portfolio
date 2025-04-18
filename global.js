359d4c7865dc44b944fe7d82059f0f317c1038bf
williamchou500 <williamchou500@gmail.com>
2025-04-17 22:45:41 -0700

    test nav

--- global.js
+++ global.js
@@ -4,12 +4,3 @@ function $$(selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
-
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-);
-
-if (currentLink) {
-    currentLink.classList.add('current');
-}
