8b289c130956d618f961d989de595273928cd87f
williamchou500 <williamchou500@gmail.com>
2025-05-07 03:43:35 -0700

    added scatterplot, need to style

--- meta/main.js
+++ meta/main.js
@@ -9,7 +9,6 @@ async function loadData() {
       date: new Date(row.date + 'T00:00' + row.timezone),
       datetime: new Date(row.datetime),
     }));
-    console.log(data);
 
     return data;
   }
@@ -45,7 +44,7 @@ function processCommits(data) {
       });
   }
 
-  function renderCommitInfo(data, commits) {
+function renderCommitInfo(data, commits) {
     // Create the dl element
     const dl = d3.select('#stats').append('dl').attr('class', 'stats');
   
@@ -77,12 +76,8 @@ function processCommits(data) {
         (v) => d3.max(v, (v) => v.line),
         (d) => d.length,
       );
-    
-    console.log(fileLengths);
 
     const maxLineLength = d3.greatest(fileLengths, (d) => d[0])[0];
-
-    console.log(maxLineLength)
 
     dl.append('dt').text('Longest Line');
     dl.append('dd').text(maxLineLength);
@@ -91,8 +86,82 @@ function processCommits(data) {
     dl.append('dd').text(d3.groups(data, d => d.file).length);
   }
   
-  let data = await loadData();
-  let commits = processCommits(data);
-  console.log(commits);
+let data = await loadData();
+let commits = processCommits(data);
   
-  renderCommitInfo(data, commits);
+renderCommitInfo(data, commits);
+
+function renderScatterPlot(data, commits) {
+    const width = 1000;
+    const height = 600;
+
+    const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .style('overflow', 'visible');
+
+    const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(commits, (d) => d.datetime))
+    .range([0, width])
+    .nice();
+
+    const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+
+    const dots = svg.append('g').attr('class', 'dots');
+    
+
+    const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+
+    const usableArea = {
+        top: margin.top,
+        right: width - margin.right,
+        bottom: height - margin.bottom,
+        left: margin.left,
+        width: width - margin.left - margin.right,
+        height: height - margin.top - margin.bottom,
+      };
+      
+    // Update scales with new ranges
+    xScale.range([usableArea.left, usableArea.right]);
+    yScale.range([usableArea.bottom, usableArea.top]);
+
+    // Add gridlines BEFORE the axes
+    const gridlines = svg
+    .append('g')
+    .attr('class', 'gridlines')
+    .attr('transform', `translate(${usableArea.left}, 0)`);
+
+    // Create gridlines as an axis with no labels and full-width ticks
+    gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+
+    // Create the axes
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3
+    .axisLeft(yScale)
+    .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+
+    // Add X axis
+    svg
+    .append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
+    // Add Y axis
+    svg
+    .append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
+
+    dots
+    .selectAll('circle')
+    .data(commits)
+    .join('circle')
+    .attr('cx', (d) => xScale(d.datetime))
+    .attr('cy', (d) => yScale(d.hourFrac))
+    .attr('r', 5)
+    .attr('fill', 'steelblue');
+}
+   
+renderScatterPlot(data, commits);
