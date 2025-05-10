2c243481865d777638ec935b3c074742693698e1
williamchou500 <williamchou500@gmail.com>
2025-05-09 22:18:02 -0700

    change loc.csv file path in code

--- meta/main.js
+++ meta/main.js
@@ -1,7 +1,7 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 async function loadData() {
-    const data = await d3.csv('loc.csv', (row) => ({
+    const data = await d3.csv('./loc.csv', (row) => ({
       ...row,
       line: Number(row.line), // or just +row.line
       depth: Number(row.depth),
